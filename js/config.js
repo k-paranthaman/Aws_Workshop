36031752a2f9f239a3fc1305b2e4fab4b131c712
EC2 Default User <ec2-user@ip-172-31-34-116.us-east-2.compute.internal>
2019-10-03 08:48:29 +0000

    enable authentication

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '68goioolsbtg0su9tnp4p1ibef', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'k7fg680n9q4qm85520hsggegjf7tq990flcu8m9d1c33u56fmt2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {

f7939dae0841efbf516fe284e245f887e75bd482
EC2 Default User <ec2-user@ip-172-31-34-116.us-east-2.compute.internal>
2019-10-03 09:00:10 +0000

    enable authentication

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: '68goioolsbtg0su9tnp4p1ibef', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'k7fg680n9q4qm85520hsggegjf7tq990flcu8m9d1c33u56fmt2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
